3825d85861fba5b14b493f105a59c775847e79c8
usamekilicbay <usame.kilicbay@gmail.com>
2022-09-08 20:06:43 +0300

    Create MirrorHub for sending real-time messages

---
+++ ECAP-Server/MirrorHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ECAP_Server;
+internal class MirrorHub : Hub
+{
+    public async Task SendMessageAsync(string username, string message)
+    {
+        if (Clients == null)
+        {
+            Console.WriteLine("There's no one to listen you, stop embarrasing yourself");
+            return;
+        }
+
+        await Clients.All.SendAsync("ListenKeyboard", username, message);
+    }
+}
