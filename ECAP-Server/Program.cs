81b14dc1e31f83946c34b9bea5c4097c082cc1a5
usamekilicbay <usame.kilicbay@gmail.com>
2022-09-08 20:07:32 +0300

    Add SignalR to services and use Routing, Endpoints

--- ECAP-Server/Program.cs
+++ ECAP-Server/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using ECAP_Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(x => x.MapHub<MirrorHub>("/listenkeyboard"));
+
+app.Run();
