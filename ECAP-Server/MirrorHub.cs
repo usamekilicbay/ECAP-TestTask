6cbecabf892741a17fba814772f704173bb532ae
usamekilicbay <usame.kilicbay@gmail.com>
2022-09-09 17:04:00 +0300

    Add comments to MirrorHub

--- ECAP-Server/MirrorHub.cs
+++ ECAP-Server/MirrorHub.cs
@@ -3,8 +3,11 @@
 namespace ECAP_Server;
 internal class MirrorHub : Hub
 {
+    // The actualy method behind the all magic, called by the client and sends message to all client
+    // For that example we can use Client.Caller as well, but maybe you want to use multiple clients. That's why I used Clients.All
     public async Task SendMessageAsync(string username, string message)
     {
+        // If Clients null don't let the SendMessage, just noticed I could use try catch here 🤦‍
         if (Clients == null)
         {
             Console.WriteLine("There's no one to listen you, stop embarrasing yourself");
