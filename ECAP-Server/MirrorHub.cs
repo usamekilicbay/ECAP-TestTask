6232e88df3ac8cbb91486fc187c51ead5bcb30e0
usamekilicbay <usame.kilicbay@gmail.com>
2022-09-10 11:44:46 +0300

    Update send message method by using try/catch

--- ECAP-Server/MirrorHub.cs
+++ ECAP-Server/MirrorHub.cs
@@ -4,17 +4,18 @@ namespace ECAP_Server;
 internal class MirrorHub : Hub
 {
     // The actualy method behind the all magic, called by the client and sends message to all client
-    // For that example we can use Client.Caller as well, but maybe you want to use multiple clients. That's why I used Clients.All
     public async Task SendMessageAsync(string username, string message)
     {
-        // If Clients null don't let the SendMessage, just noticed I could use try catch here 🤦‍
-        if (Clients == null)
+        try
+        {
+            Console.Write(message);
+
+            // For that example we can use Client.Caller as well, but maybe you want to use multiple clients. That's why I used Clients.All
+            await Clients.All.SendAsync("ListenKeyboard", username, message);
+        }
+        catch (Exception)
         {
             Console.WriteLine("There's no one to listen you, stop embarrasing yourself");
-            return;
         }
-
-        Console.Write(message);
-        await Clients.All.SendAsync("ListenKeyboard", username, message);
     }
 }
