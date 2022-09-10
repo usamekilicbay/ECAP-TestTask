2efe04a77e6cf18a278d592b9496f76025582140
usamekilicbay <usame.kilicbay@gmail.com>
2022-09-10 11:12:07 +0300

    Create ServerSettings class

---
+++ ECAP-Client/ServerSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace ECAP_Client;
+internal class ServerSettings
+{
+    public ServerSettings(string ip, string port)
+    {
+        Ip = ip;
+        Port = port;
+    }
+
+    public string Ip { get; private set; } = "localhost";
+    public string? Port { get; private set; }
+}
