80059c992a31f744f09bc5554fd8d7e31d84787f
usamekilicbay <usame.kilicbay@gmail.com>
2022-09-10 11:13:03 +0300

    Create services for IoC and configurations

--- ECAP-Client/Program.cs
+++ ECAP-Client/Program.cs
@@ -1,6 +1,19 @@
 ﻿using ECAP_Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 Console.WriteLine("Press \"Enter\" to kill my masterpiece...");
 
-var receiver = new ClientManager();
+var services = new ServiceCollection();
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+services.AddSingleton(configuration);
+services.AddSingleton<ClientManager>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var receiver = serviceProvider.GetService<ClientManager>();
 await receiver.ContinuousTyping();
